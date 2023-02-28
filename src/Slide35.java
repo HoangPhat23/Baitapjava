577ca8c99cedbfd8d0bd49b2023308832ab42e45
hqphat <hqphat203@gmail.com>
2023-02-28 14:41:04 +0700

    update

--- src/Slide35.java
+++ src/Slide35.java
@@ -1,20 +1,17 @@
-import java.util.Scanner;
+import java.util.Scanner; 
 
 public class Slide35 {
-    public static void main(String[] args) throws Exception {
-        int a, b;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("nhap so a:");
-        a = sc.nextInt();
-        System.out.println("nhap so b:");
-        b = sc.nextInt();
-        if (a > b) {
-            System.out.println("So nho hon la: " + b);
-        } else if (a < b) {
-            System.out.println("So nho hon la: " + a);
-        } else {
-            System.out.println("Hai so bang nhau");
-
-        }
-
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Nhap vao 2 so nguyen: ");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    if(a >= b){
+        System.out.println("so nho nhat trong 2 so la " + b);
     }
+    else{
+        System.out.println("so nho nhat trong 2 so la " + a);
+    }
+    
+   } 
+}

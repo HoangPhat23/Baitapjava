415ef9cc3bf5e62282be9cb7727073859dc8b8ef
hqphat <hqphat203@gmail.com>
2023-03-04 12:57:32 +0700

    add new project

---
+++ src/Baithuchanh0303/slide47.java
@@ -0,0 +1,24 @@
+package Baithuchanh0303;
+
+import java.util.Scanner;
+
+public class slide47 {
+   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int tong = 0 ;
+    int n;
+    String s ="";
+    do{
+        System.out.println("vui long nhap vao so nguyen");
+         n = scan.nextInt();
+         s = s +n +" ";
+        tong+=n;
+    }while(tong<=100);
+    String v = s.trim().replace(" ", " + ");
+    System.out.println("tong cac so vua nhap la: "+ v+" = "+tong);
+
+   }
+
+    
+    
+}
